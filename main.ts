5db9a289a5886116454b5eba4c413fba5ad82bf3
anilaahmed1987 <155479535+anilaahmed1987@users.noreply.github.com>
2024-07-09 22:59:50 +0500

    now cli calculator is ready for working

---
+++ main.ts
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+
+// CLI CALCULATOR ramzan coding night
+import inquirer from "inquirer";
+// ASking question from user through inquirer
+let answer = await inquirer.prompt([
+    {
+        message:"enter first Number",type:"number",name:"firstNumber"} ,
+    {message:"enter second Number",type:"number",name:"secondNumber"},
+    {message:"select one operator to perform operations",
+    type:"list",
+    name:"operator",
+    choices:["addition","subtraction","multiplication","division","percentage"]
+
+    },
+
+])
+// coditional statements if-else
+if(answer.operator==="addition"){
+console.log(answer.firstNumber+answer.secondNumber)
+}
+else if(answer.operator==="subtraction"){
+    console.log(answer.firstNumber-answer.secondNumber)
+}
+else if(answer.operator==="multiplication"){
+    console.log(answer.firstNumber * answer.secondNumber)
+}
+else if(answer.operator==="division"){
+    console.log(answer.firstNumber / answer.secondNumber)
+}
+else if(answer.operator==="percentage"){
+    console.log(answer.firstNumber % answer.secondNumber)
+}
+else{
+console.log("invalid out  put")
+}
+
